bd22488b566028e9f391750fc988691ade2c4cd6
level09 <level09@gmail.com>
2025-05-08 22:44:57 +0300

    Replace default templates with black-themed website design

--- assets/default-templates/src/main.js
+++ assets/default-templates/src/main.js
@@ -1,7 +1,4 @@
-// Import Tailwind CSS
-import './style.css';
+import './style.css'
 
-// Main JavaScript code
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Vilcos template loaded successfully!');
-}); 
+// Any additional JavaScript functionality can be added here
+console.log('Vilcos template initialized') 
